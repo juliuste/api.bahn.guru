a8a5a3a2ce7584f13254611653f455338888d649
Julius Tens <mail@juliustens.eu>
2020-08-27 02:29:30 +0200

    enable CORS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const http = require('http')
 const compression = require('compression')
+const cors = require('cors')
 
 const router = require('./routes')
 
@@ -10,6 +11,7 @@ const api = express()
 const server = http.createServer(api)
 
 api.use(compression())
+api.use(cors())
 api.use(router)
 
 const getEnv = name => {
